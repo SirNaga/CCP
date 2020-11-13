f304f63d1a8cb72a720a220b633ffc5155607333
unknown <cislo.lukas@hotmail,com>
2020-11-13 09:49:37 +0100

    corrected path to test

--- servicediscovery/serviceDiscovery.py
+++ servicediscovery/serviceDiscovery.py
@@ -2,31 +2,33 @@ import exoscale
 import os
 import time as timer
 
-
+#setting enviromental variables
 os.environ["EXOSCALE_API_KEY"] = os.environ["EXOSCALE_KEY"]
 os.environ["EXOSCALE_API_SECRET"] = os.environ["EXOSCALE_SECRET"]
 instancePoolId = os.environ['EXOSCALE_INSTANCEPOOL_ID']
 zone = os.environ['EXOSCALE_ZONE']
 targetPort = os.environ['TARGET_PORT']
-preffix = "[{\"targets\": [ "
-suffix = " ]}]"
-
 
 while True:
+    # Get Exoscale instance
+    exoscaleConnection = exoscale.Exoscale()
+    exoscaleZone = exoscaleConnection.compute.get_zone(zone)
 
-        exoscaleConnection = exoscale.Exoscale()
-        exoscaleZone = exoscaleConnection.compute.get_zone(zone)
-        instances = list(exoscaleConnection.compute.get_instance_pool(instancePoolId, exoscaleZone).instances)
-        instanceIPv4Addresses = list()
+    instances = list(exoscaleConnection.compute.get_instance_pool(instancePoolId, exoscaleZone).instances)
+    instanceIPv4Addresses = list()
 
-        for instance in instances:
-                if instance.state == "running":
-                        instanceIPv4Addresses.append("\"" + instance.ipv4_address + ":" + str(targetPort) + "\"")
+    for instance in instances:
+        if instance.state == "running":
+            instanceIPv4Addresses.append("\"" + instance.ipv4_address + ":" + str(targetPort) + "\"")
 
-        targetsFile = open("/prometheus/targets.json", "w")
-        targetsFile.write(preffix + (", ".join(instanceIPv4Addresses)) + suffix)
-        targetsFile.close()
-        targetsFileConfig = open("/srv/service-discovery/config.json", "w")
-        targetsFileConfig.write(preffix + (", ".join(instanceIPv4Addresses)) + suffix)
-        targetsFileConfig.close()
-        timer.sleep(10)
+    # Write file for prometheus targets
+    targetsFile = open("/prometheus/targets.json", "w")
+    targetsFile.write("[{\"targets\": [" + (", ".join(instanceIPv4Addresses)) + "]}]")
+    targetsFile.close()
+
+    # Write file for required path
+    targetsFileConfig = open("/srv/service-discovery/config.json", "w")
+    targetsFileConfig.write("[{\"targets\": [" + (", ".join(instanceIPv4Addresses)) + "]}]")
+    targetsFileConfig.close()
+
+    timer.sleep(10)
